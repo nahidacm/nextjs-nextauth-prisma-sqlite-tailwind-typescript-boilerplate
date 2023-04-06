24502fd3c702bb32355d1fb4f8587e0bfadc59c9
Mahmudur Rahman <nahidacm@gmail.com>
2023-04-06 16:03:10 +0600

    Not sure if login is working

---
+++ components/login.jsx
@@ -0,0 +1,53 @@
+import { signIn } from "next-auth/react";
+import { useState } from "react";
+
+export default function Login() {
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    const result = await signIn("credentials", {
+      email: formData.email,
+      password: formData.password,
+      redirect: false,
+    });
+
+    if (result?.error) {
+      console.error("Failed to log in:", result.error);
+    }
+  };
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setFormData((prevState) => ({ ...prevState, [name]: value }));
+  };
+
+  return (
+    <div>
+      <h1>Login</h1>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="email">Email</label>
+        <input
+          type="email"
+          id="email"
+          name="email"
+          value={formData.email}
+          onChange={handleChange}
+        />
+        <label htmlFor="password">Password</label>
+        <input
+          type="password"
+          id="password"
+          name="password"
+          value={formData.password}
+          onChange={handleChange}
+        />
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+}
--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,3 +1,4 @@
+import { compare } from "bcrypt";
 import NextAuth from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
 import { PrismaAdapter } from "@next-auth/prisma-adapter"
@@ -9,33 +10,45 @@ export const authOptions = {
   // Configure one or more authentication providers
   providers: [
     CredentialsProvider({
-      // The name to display on the sign in form (e.g. "Sign in with...")
       name: "Credentials",
-      // `credentials` is used to generate a form on the sign in page.
-      // You can specify which fields should be submitted, by adding keys to the `credentials` object.
-      // e.g. domain, username, password, 2FA token, etc.
-      // You can pass any HTML attribute to the <input> tag through the object.
       credentials: {
-        email: {
-          label: "emial",
-          type: "email",
-          placeholder: "jsmith@example.com",
-        },
+        email: { label: "Email", type: "email", placeholder: "email@example.com" },
         password: { label: "Password", type: "password" },
       },
-      async authorize(credentials, req) {
-        const { email, password } = req.body;
+      async authorize(credentials) {
+        const { email, password } = credentials;
 
         const user = await prisma.user.findUnique({ where: { email } });
 
-        if (!user || user.password !== password) {
-          return null;
+        if (!user) {
+          throw new Error("Invalid email or password");
+        }
+
+        const passwordMatch = await compare(password, user.password);
+
+        if (!passwordMatch) {
+          throw new Error("Invalid email or password");
         }
 
         return user;
       },
     }),
   ],
+  pages: {
+    signIn: "/login",
+  },
+  callbacks: {
+    async jwt(token, user) {
+      if (user) {
+        token.userId = user.id;
+      }
+      return token;
+    },
+    async session(session, token) {
+      session.userId = token.userId;
+      return session;
+    },
+  },
   adapter: PrismaAdapter(prisma),
 };
 
